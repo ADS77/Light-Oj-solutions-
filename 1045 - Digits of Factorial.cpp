2ca8f666fd2366578d7dc6bba986a0bdc5d45cca
Aunkur Das Shanto <45828511+ADS77@users.noreply.github.com>
2020-07-13 21:45:13 +0600

    Update 1045 - Digits of Factorial.cpp

--- 1045 - Digits of Factorial.cpp
+++ 1045 - Digits of Factorial.cpp
@@ -1,3 +1,23 @@
+/*
+ n! means successive multiplication on n numbers from 1-n  i.e n!= 1*2*3*4……..*n
+We know,
+log(a*b) = log(a) + log(b)
+
+Therefore
+log( n! ) = log(1*2*3....... * n) 
+          = log(1) + log(2) + ........ +log(n)
+
+Now, observe that the floor value of log base 
+10 increased by 1, of any number, gives the
+number of digits present in that number.
+
+Hence, output would be : floor(log(n!)) + 1.
+However that solution would not be able to handle cases where n >10^6
+Kamenetsky's formula is the solution 
+f(n) = log10((n/e)^n√(2πn))
+f(n) = nlog10(n/e) + log10(2πn)/2
+log(n)/log(base) gives the value of log(n) in particular base
+*/
  #include <bits/stdc++.h>
 using namespace std;
 //#define long long ll
